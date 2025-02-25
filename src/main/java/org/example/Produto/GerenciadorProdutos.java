b59b7d5025c766e3c5f3082079220f31c7f11ea6
leandrochs <leandrochagask@gmail.com>
2025-02-24 22:05:52 -0300

    adidiona gerenciador para criacao de produtos

---
+++ src/main/java/org/example/Produto/GerenciadorProdutos.java
@@ -0,0 +1,24 @@
+package org.example.Produto;
+
+import org.example.BancoDeDados.BancoDeDadosProdutos;
+
+public class GerenciadorProdutos {
+    public static void criarProduto(
+            String identificador,
+            String nome,
+            String categoria,
+            double valorProduto,
+            double valorVenda,
+            double valorDesconto) {
+        Produto novoProduto = new Produto(
+                identificador,
+                nome,
+                categoria,
+                valorProduto,
+                valorVenda,
+                valorDesconto);
+
+        BancoDeDadosProdutos.adicionaProdutos(novoProduto);
+        System.out.println("Produto adicionado com sucesso!");
+    }
+}
